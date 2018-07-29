95e773ce834e6bbf045d80c38fc24a7d02dc7705
Eashwar Mohan <eashwar@umich.edu>
2018-07-28 21:51:20 -0400

    Reduce image display time

--- src/main.cpp
+++ src/main.cpp
@@ -22,7 +22,7 @@ int main( int argc, char* argv[] )
 
     SDL_BlitSurface( image_surface, NULL, window_surface, NULL );
     SDL_UpdateWindowSurface( window );
-    SDL_Delay( 6000 );
+    SDL_Delay( 4000 );
 
     CleanupAndQuit( window, image_surface );
     return 0;
